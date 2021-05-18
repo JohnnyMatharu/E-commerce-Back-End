2b5029d97a6dcad8bafb03d0ae2b87c2d27c6b09
Johnny <jaani.matharu@gmail.com>
2021-05-18 19:09:41 -0400

    test .env

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -33,7 +33,7 @@ router.delete('/:id', (req, res) => {
 
 module.exports = router;
 
-
+//installed sql2, sequelize, dotenv, rest could be seen through dependencies
 // api/categories call this this.route, routes are made already
  //finish models, and routes in api folder, nothing server.js, put in insomnia in end to test  
  
@@ -66,3 +66,22 @@ Notes in server.js
 
 //routes to index to api, to index to express, api routes use models using sequelize and return
 //models to model database and seeds to fill value , npm i installed, check dependencies if needed
+/*
+Acceptance Criteria
+GIVEN a functional Express.js API
+
+HERE
+
+WHEN I add my database name, MySQL username, and MySQL password to an
+environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL
+database
+WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete data in my database
+*/
