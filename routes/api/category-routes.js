84309e5b83df127956a1e6ff5c54c80f6aed16d9
Johnny <jaani.matharu@gmail.com>
2021-05-20 18:43:12 -0400

    Category routes working except delete to be checked

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,25 +24,71 @@ router.get('/', (req, res) => {
  })
   // find all categories
   // be sure to include its associated Products
- 
 });
 
+
 router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+  
+    res.json(data)
+  
+   })
+   //to be checked on insomnia
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
+
+
 router.post('/', (req, res) => {
   // create a new category
-});
+    Category.create({
+    id : req.body.id,
+    category_name: req.body.category_name
+    }).then(data => {
+  
+      res.json(data)
+    
+     })
+     //to be checked on insomnia and syntax for expectation needs to be too
+  });
+
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(data => {
+  
+    res.json(data)
+  
+   })
+   //to be checked on insomnia
 });
+
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-});
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(data => {
+  
+      res.json(data)
+    
+     })
+     //to be checked on insomnia
+  });
 
 module.exports = router;
 
@@ -54,9 +100,6 @@ module.exports = router;
 /*
 Hilary:
 Insomnia
-Or
-Postman
-Postman is easier, both almost same, if using mention in submission 
 
 You can use table plus application if needed
 
